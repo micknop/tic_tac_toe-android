746b7106c49f927db3224903036b877c69098b50
micknop <Michel.Knop@gmx.de>
2016-09-30 16:23:58 +0200

    This abstract game logic implements methods which are independent from any inner representations and computation steps of explicit game logic classes. This is validation and loading of a continuable game state, as well as the construction of a printable game state (String-representation).

---
+++ app/src/main/java/com/neopoly/tictactoe/gamelogic/AbstractTicTacToeGameLogic.java
@@ -0,0 +1,89 @@
+package com.neopoly.tictactoe.gamelogic;
+
+/**
+ * This abstract game logic class for TicTacToe-games implements some comfort methods which can be
+ * performed independent from any inner representations and computation steps of explicit game logic
+ * classes. In detail this is validation and following loading of game states, as well as the
+ * construction of printable game state representations.
+ */
+public abstract class AbstractTicTacToeGameLogic implements TicTacToeGameLogicInterface {
+
+    @Override
+    public String getPrintableGameState(Character firstGamer, Character secondGamer) {
+
+        Character[] usableChars = new Character[] {firstGamer, CHAR_EMPTY_FIELD, secondGamer};
+        Character[] fieldChars = new Character[] {TOTAL_FIELD_COUNT};
+        int[] gameState = getGameState();
+        for (int field = 0; field < TOTAL_FIELD_COUNT; field++) {
+            fieldChars[field] = usableChars[gameState[field] + 1];
+        }
+
+        return " \n " + fieldChars[0] + " | " + fieldChars[1] + " | " + fieldChars[2] + " \n"
+                + "---|---|---\n"
+                + " " + fieldChars[3] + " | " + fieldChars[4] + " | " + fieldChars[5] + " \n"
+                + "---|---|---\n"
+                + " " + fieldChars[6] + " | " + fieldChars[7] + " | " + fieldChars[8] + " \n";
+    }
+
+    @Override
+    public int startFromGameState(int[] gameState) throws IllegalGameStateException{
+
+        int nextGamer = FIRST_GAMER;
+        startNewGame();
+
+        if (gameState != null && gameState.length == TOTAL_FIELD_COUNT  // Check number of fields
+                && gameState[0] >= -1 && gameState[0] <= 1
+                && gameState[1] >= -1 && gameState[1] <= 1
+                && gameState[2] >= -1 && gameState[2] <= 1
+                && gameState[3] >= -1 && gameState[3] <= 1      // Check that each field's value
+                && gameState[4] >= -1 && gameState[4] <= 1      // equals one of the three values:
+                && gameState[5] >= -1 && gameState[5] <= 1      // '-1', '0' or '+1'
+                && gameState[6] >= -1 && gameState[6] <= 1
+                && gameState[7] >= -1 && gameState[7] <= 1
+                && gameState[8] >= -1 && gameState[8] <= 1) {
+
+            // Collect the indices of all fields flagged by the first an the second gamer
+            int[] fieldIndicesOfFirstGamer = new int[] {4};
+            int[] fieldIndicesOfSecondGamer = new int[] {4};
+            int fieldCountOfFirstGamer = 0;
+            int fieldCountOfSecondGamer = 0;
+            int currentFieldValue;
+            for (int fieldIndex = 0; fieldIndex < TOTAL_FIELD_COUNT; fieldIndex++) {
+                currentFieldValue = gameState[fieldIndex];
+                if (currentFieldValue == FIRST_GAMER) {
+                    fieldIndicesOfFirstGamer[fieldCountOfFirstGamer] = fieldIndex;
+                    fieldCountOfFirstGamer++;
+                } else if (currentFieldValue == SECOND_GAMER) {
+                    fieldIndicesOfSecondGamer[fieldCountOfSecondGamer] = fieldIndex;
+                    fieldCountOfSecondGamer++;
+                }
+            }
+
+            // Check if the number of flagged fields is either equal or the first gamer has one more
+            if (fieldCountOfFirstGamer == fieldCountOfSecondGamer
+                    || fieldCountOfFirstGamer == (fieldCountOfSecondGamer + 1)) {
+                // Try to insert all flags into the new game - order is irrelevant for finally state
+                try {
+                    int returnValue = 0;
+                    for (int round = 0; round < fieldCountOfSecondGamer; round++) {
+                        setSignToField(fieldIndicesOfFirstGamer[round]);
+                        returnValue = setSignToField(fieldIndicesOfSecondGamer[round]);
+                    }
+                    if (fieldCountOfFirstGamer > fieldCountOfSecondGamer) {
+                        returnValue = setSignToField(
+                                fieldIndicesOfFirstGamer[fieldCountOfFirstGamer - 1]);
+                        nextGamer = SECOND_GAMER;
+                    }
+                    if (returnValue != 0) {
+                        throw new IllegalGameStateException();
+                    }
+                } catch (Exception e) {
+                    throw new IllegalGameStateException();
+                }
+            } else throw new IllegalGameStateException();
+
+        } else throw new IllegalGameStateException();
+
+        return nextGamer;
+    }
+}
